727ecdc7dfa675cc40bace564c8e0a986ac74c77
AvgustChen <avchen@yandex.ru>
2022-07-12 12:04:37 +0300

    Добавлено решение первой задачи

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Number(int number)
+{
+    int num = number / 10 % 10;
+    return num;
+}
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Number(number);
+
+Console.WriteLine(result);
+
